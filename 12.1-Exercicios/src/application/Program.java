b62d6973827a80febb646230a58842c1c13f088e
Renato <rena.f.oliveira@gmail.com>
2020-12-09 22:05:13 -0300

    Sobre Lista e seus métodos

---
+++ 12.1-Exercicios/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("How many employees will be registered?");
+		System.out.println();
+		String workes = sc.nextLine();
+		
+		Employee x;
+		
+		List<>
+		
+		
+		sc.close();
+	}
+
+}
